3c360a9b0f452a2d1efff937161368bbd63aa240
Kelvin Zimmerman <71656069+afterglow79@users.noreply.github.com>
2026-04-24 16:46:41 +0000

    begin work on audio gen

--- gen/tts.py
+++ gen/tts.py
@@ -1,2 +1,15 @@
-def begin(NVIDIA_API_KEY): ## LOOK INTO EDGETTS LATER, MAYBE USE FOR TTS IN PLACE OF AN AI MODEL
-    print("a") # TODO
+import asyncio
+import edge_tts
+
+OUTPUT_FILE = "voice.mp3"
+SRT_FILE = "voice.srt"
+
+def begin(NVIDIA_API_KEY, TEXT):
+    asyncio.run(amain(TEXT))
+
+async def amain(TEXT) -> None:
+    voices = await VoicesManger.create()
+    voice = voices.find(Gender = "Male", Language = "en-US")
+    print(f"Generating mp3/srt using voice: {voice.Name}")
+
+    tts = edge_tts.Communicate(TEXT, voice.Name)
